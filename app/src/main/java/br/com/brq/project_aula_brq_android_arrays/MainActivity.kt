0305b96b6ff62fe36b70780ae379412101d0024e
Rodolfo_Araujo_Vanucchi <rodolfo_vanucchi@hotmail.com>
2021-05-08 20:11:00 -0300

    first commit

---
+++ app/src/main/java/br/com/brq/project_aula_brq_android_arrays/MainActivity.kt
@@ -0,0 +1,11 @@
+package br.com.brq.project_aula_brq_android_arrays
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
