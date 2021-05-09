3e18c371524f4b135d7aa1807a2b10e79b979cc2
Rodolfo_Araujo_Vanucchi <rodolfo_vanucchi@hotmail.com>
2021-05-08 21:58:57 -0300

    Commit 6

--- app/src/main/java/br/com/brq/project_aula_brq_android_arrays/MainActivity.kt
+++ app/src/main/java/br/com/brq/project_aula_brq_android_arrays/MainActivity.kt
@@ -2,13 +2,21 @@ package br.com.brq.project_aula_brq_android_arrays
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var recycler: RecyclerView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        lateinit var nome:String
+        carregarElementos()
 
+    }
+
+    fun carregarElementos(){
+        recycler = findViewById(R.id.reciclador)
     }
 }
--- app/src/main/java/br/com/brq/project_aula_brq_android_arrays/example_item.kt
+++
@@ -1,3 +0,0 @@
-package br.com.brq.project_aula_brq_android_arrays
-
-data class example_item(val imageResource: Int, val text1: String, val text2: String)
